495c6e34145448e5f18684e9a9b554974828d621
Gustavo Caldas de Souza <133917173+gustavocaldassouza@users.noreply.github.com>
2023-11-28 11:28:28 -0300

    Angular from scratch

--- FlashTalk.UI/src/app/app.component.ts
+++ FlashTalk.UI/src/app/app.component.ts
@@ -1,60 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { CardModule } from 'primeng/card';
-import { ButtonModule } from 'primeng/button';
-import { PrimeNGConfig } from 'primeng/api';
-import { CheckboxModule } from 'primeng/checkbox';
-import { StyleClassModule } from 'primeng/styleclass';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    CardModule,
-    ButtonModule,
-    CheckboxModule,
-    StyleClassModule,
-    HttpClientModule,
-  ],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  constructor(private primengConfig: PrimeNGConfig, private http: HttpClient) {}
-
-  ngOnInit() {
-    this.primengConfig.ripple = true;
-    this.primengConfig.zIndex = {
-      modal: 1100, // dialog, sidebar
-      overlay: 1000, // dropdown, overlaypanel
-      menu: 1000, // overlay menus
-      tooltip: 1100, // tooltip
-    };
-    this.getData();
-    this.postData();
-  }
-
-  getData() {
-    this.http.get('/api/weatherforecast').subscribe(
-      (data: any) => console.log(data),
-      (error: any) => console.error(error)
-    );
-  }
-
-  postData() {
-    this.http
-      .post('/api/userregistration', {
-        name: 'string',
-        email: 'stringstringstringst',
-        password: 'string',
-      })
-      .subscribe(
-        (data: any) => console.log(data),
-        (error: any) => console.error(error)
-      );
-  }
+export class AppComponent {
+  title = 'FlashTalk.UI';
 }
