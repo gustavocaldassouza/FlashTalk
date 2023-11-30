c10b7c6f20cbfbdb689542fac084c45fbbae8ba6
Gustavo Caldas de Souza <133917173+gustavocaldassouza@users.noreply.github.com>
2023-11-30 02:21:24 -0300

    Refactor Channel component to use dynamic rendering

--- FlashTalk.UI/src/components/Channel.tsx
+++ FlashTalk.UI/src/components/Channel.tsx
@@ -7,46 +7,52 @@ interface ChannelProps {
   chat: Chat;
 }
 
+const numbers = [1, 2, 3];
+
 export default function Channel({ chat }: ChannelProps) {
   return (
     <Stack spacing={1}>
       <ChannelBar chat={chat}></ChannelBar>
-      <Box height={238}>
-        <Box>
-          <Box
-            maxWidth="50%"
-            width="25%"
-            height={37}
-            margin={1}
-            sx={{
-              backgroundColor: "#F5F5F5",
-              color: "black",
-              borderRadius: "10px 10px 10px 0px",
-            }}
-          >
-            <Typography paddingTop="8px" paddingLeft="10px">
-              Hello there!
-            </Typography>
+      <Box height={238} overflow={"auto"}>
+        {numbers.map((number) => (
+          <Box key={number}>
+            <Box>
+              <Box
+                maxWidth="50%"
+                width="25%"
+                height={37}
+                margin={1}
+                sx={{
+                  backgroundColor: "#F5F5F5",
+                  color: "black",
+                  borderRadius: "10px 10px 10px 0px",
+                }}
+              >
+                <Typography paddingTop="8px" paddingLeft="10px">
+                  Hello there!
+                </Typography>
+              </Box>
+            </Box>
+            <Box>
+              <Box
+                maxWidth="50%"
+                width="25%"
+                height={37}
+                margin={1}
+                sx={{
+                  backgroundColor: "#1976D2",
+                  borderRadius: "10px 10px 0px 10px",
+                  marginLeft: "auto",
+                  color: "white",
+                }}
+              >
+                <Typography paddingTop="8px" paddingLeft="10px">
+                  Hey!
+                </Typography>
+              </Box>
+            </Box>
           </Box>
-        </Box>
-        <Box>
-          <Box
-            maxWidth="50%"
-            width="25%"
-            height={37}
-            margin={1}
-            sx={{
-              backgroundColor: "#1976D2",
-              borderRadius: "10px 10px 0px 10px",
-              marginLeft: "auto",
-              color: "white",
-            }}
-          >
-            <Typography paddingTop="8px" paddingLeft="10px">
-              Hey!
-            </Typography>
-          </Box>
-        </Box>
+        ))}
       </Box>
       <Box sx={{ backgroundColor: "#f5f5f5" }}>
         <InputBase
