b755f6b7281bfac9a0e502d5cb50ac0000496d98
Gustavo Caldas de Souza <133917173+gustavocaldassouza@users.noreply.github.com>
2023-11-30 02:15:44 -0300

    Add chat input and send button

--- FlashTalk.UI/src/components/Channel.tsx
+++ FlashTalk.UI/src/components/Channel.tsx
@@ -1,6 +1,7 @@
-import { Box } from "@mui/material";
+import { Box, IconButton, InputBase, Stack, Typography } from "@mui/material";
 import ChannelBar from "./ChannelBar";
 import { Chat } from "../models/Chat";
+import SendIcon from "@mui/icons-material/Send";
 
 interface ChannelProps {
   chat: Chat;
@@ -8,8 +9,55 @@ interface ChannelProps {
 
 export default function Channel({ chat }: ChannelProps) {
   return (
-    <Box>
+    <Stack spacing={1}>
       <ChannelBar chat={chat}></ChannelBar>
-    </Box>
+      <Box height={238}>
+        <Box>
+          <Box
+            maxWidth="50%"
+            width="25%"
+            height={37}
+            margin={1}
+            sx={{
+              backgroundColor: "#F5F5F5",
+              color: "black",
+              borderRadius: "10px 10px 10px 0px",
+            }}
+          >
+            <Typography paddingTop="8px" paddingLeft="10px">
+              Hello there!
+            </Typography>
+          </Box>
+        </Box>
+        <Box>
+          <Box
+            maxWidth="50%"
+            width="25%"
+            height={37}
+            margin={1}
+            sx={{
+              backgroundColor: "#1976D2",
+              borderRadius: "10px 10px 0px 10px",
+              marginLeft: "auto",
+              color: "white",
+            }}
+          >
+            <Typography paddingTop="8px" paddingLeft="10px">
+              Hey!
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+      <Box sx={{ backgroundColor: "#f5f5f5" }}>
+        <InputBase
+          sx={{ ml: 1, flex: 1, width: "calc(100% - 60px)" }}
+          placeholder="Type your message"
+          inputProps={{ "aria-label": "search google maps" }}
+        />
+        <IconButton type="button" sx={{ p: "10px" }} aria-label="delete">
+          <SendIcon fontSize="small" />
+        </IconButton>
+      </Box>
+    </Stack>
   );
 }
